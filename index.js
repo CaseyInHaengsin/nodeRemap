f5bfe44ca895f2f1cb97e0d8a04843248cf6816b
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-02-14 09:30:02 -0700

    adjusting code to use functions.

--- index.js
+++ index.js
@@ -15,47 +15,49 @@ const newPath = process.env.TEST_FILE_PATH2
 
 const match_val = 'course_id';
 
-let fullAnalysis = async (newRecord, canvasRecord) => {
-        let keys = Object.keys(newDataArr);
-        let recordArr = [];
-        keys.forEach((key) => {
+// let fullAnalysis = async (newRecord, canvasRecord) => {
+//         let keys = Object.keys(newDataArr);
+//         let recordArr = [];
+//         keys.forEach((key) => {
             
-        });
-};
+//         });
+// };
 
+const analyzeStep1 = async (newDatacsv, canvasDataCsv, keys) => {
+    newDatacsv.forEach((newRecord) => {
+        let test = canvasDataCsv.find((canvasRecord) => {
+            let allCheck = [];
+                keys.forEach((key) => {
+                    if (newRecord[key].toLowerCase() == canvasRecord[key].toLowerCase()){
+                        
+                        allCheck.push({
+                            attribute: key,
+                            shared: true
+                        })
+                    }
+                    
+                });
+                
+                //TODO - Push all check shared 
+                let allCheckShared = [];
+                allCheck.forEach((thing) => {
+                    allCheckShared.push(thing.shared);
+                    console.log(thing.attribute);
+                })
+                
+        });
+    });
+}
 
 const readCsvs = async (fPathCanvas, fPathNew, matchVal) => {
     const newData = await csv().fromFile(fPathNew);
+    const keys = Object.keys(newData[0]);
     const canvasData = await csv().fromFile(fPathCanvas);
-    const keys = Object.keys(canvasData[0]);
-    
-    newData.forEach((newRecord) => {
-        let test = canvasData.find((canvasRecord) => {
-            // console.log(canvasRecord[matchVal]);
-            // console.log(newRecord[matchVal]);
-            //return newRecord[matchVal] == canvasRecord[matchVal];
-            let allCheck = [];
-            keys.forEach((key) => {
-                if (newRecord[key].toLowerCase() == canvasRecord[key].toLowerCase()){
-                    
-                    allCheck.push({
-                        attribute: key,
-                        shared: true
-                    })
-                }
-                
-            })
-            //TODO - Figure out why this check isn't passing for current test data
-            if (!allCheck.shared.includes(false)){
-                allMatched.push(newRecord);
-            }
-        })
-        
+    const r = await canvasData
 
-        if (test != undefined){
-            console.log(test);
-        }
-    })
+    console.log(r);
+    analyzeStep1(newData, r, keys);
+
 }
 
 
